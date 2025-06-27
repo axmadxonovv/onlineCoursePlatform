0e5e5f33a87c1dba02bb4bee842e818fcb2cc37b
Avazbek <apple@MacBook-Pro-apple.local>
2025-06-27 17:48:39 +0500

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(cookieParser());
+
+  // Swagger konfiguratsiyasi
+  const config = new DocumentBuilder()
+    .setTitle('Online Course API')
+    .setDescription('Online kurs platformasining API hujjati')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
   await app.listen(3000);
 }
 bootstrap();
